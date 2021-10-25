a017e2a935e4b167f3aaddcb1645f8399c5c8dc4
cs19b005-gautam <bgautam1222@gmail.com>
2021-10-25 12:57:35 +0530

    components to family added

--- client/src/pages/family/FamilyDashboard.js
+++ client/src/pages/family/FamilyDashboard.js
@@ -1,21 +1,15 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
 import './family.css'
 
 export default function FamilyDashboard() {
     return (
-        <div>
-            <div class="row">
-            <div class="buttons-column">
-                <a href="#" class="">My Family Health</a>
-                <a href="#" class="">Notifications</a>
-                <a href="#" class="">Donate Money</a>
-                <a href="#" class="">Donate Senior Citizen Requirements</a>
-                <a href="#" class="">Support Volunteer</a>
+        <div className="family-dash-board">
+            <Sidebar/>
+            <div>
+                <h1>hello</h1>
+                <h2>HI</h2>
             </div>
-            <div class="text-column">
-                <h1>Our Web Page info here</h1>
-            </div>
-        </div>
         </div>
     )
 }
