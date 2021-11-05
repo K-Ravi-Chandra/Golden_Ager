ab2398d7f162d4df296662042db76978420b79a4
Adithya <cs19b019@iittp.ac.in>
2021-11-06 01:09:38 +0530

    Updates

---
+++ client/src/pages/volunterr_dashboard/components/Financialrequests.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const financialrequests = () => {
+    return (
+        <div>
+            <h1>This is financial requests page</h1>
+        </div>
+    )
+}
+
+export default financialrequests
