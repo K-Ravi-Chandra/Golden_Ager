d0e7b53b3397d393a38520d47b76e4c50c23fa21
AdityaCS19B019 <cs19b019@iittp.ac.in>
2021-11-12 19:35:25 +0530

    History and Help requests refreshed

--- client/src/pages/volunterr_dashboard/components/HelpRequests.js
+++ client/src/pages/volunterr_dashboard/components/HelpRequests.js
@@ -1,66 +1,225 @@
 import React from 'react'
-import { DataGrid } from '@mui/x-data-grid';
-const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-      field: 'Name',
-      headerName: 'Name',
-      width: 150,
-      editable: true,
+import {format} from 'date-fns'
+import { v4 as uuid } from 'uuid';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Tooltip
+} from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+
+const SeverityPillRoot = styled('span')(({ theme, ownerState }) => {
+  const backgroundColor = theme.palette[ownerState.color].main;
+  const color = theme.palette[ownerState.color].contrastText;
+
+  return {
+    alignItems: 'center',
+    backgroundColor,
+    borderRadius: 12,
+    color,
+    cursor: 'default',
+    display: 'inline-flex',
+    flexGrow: 0,
+    flexShrink: 0,
+    fontFamily: theme.typography.fontFamily,
+    fontSize: theme.typography.pxToRem(12),
+    lineHeight: 2,
+    fontWeight: 600,
+    justifyContent: 'center',
+    letterSpacing: 0.5,
+    minWidth: 20,
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    textTransform: 'uppercase',
+    whiteSpace: 'nowrap'
+  };
+});
+
+const SeverityPill = (props) => {
+  const { color = 'primary', children, ...other } = props;
+
+  const ownerState = { color };
+
+  return (
+    <SeverityPillRoot
+      ownerState={ownerState}
+      {...other}
+    >
+      {children}
+    </SeverityPillRoot>
+  );
+};
+
+SeverityPill.propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.oneOf([
+    'primary',
+    'secondary',
+    'error',
+    'info',
+    'warning',
+    'success'
+  ])
+};
+
+
+const orders = [
+  {
+    id: uuid(),
+    ref: 'CDD1049',
+    amount: 30.5,
+    customer: {
+      name: 'Ekaterina Tankova'
     },
-    {
-      field: 'Address',
-      headerName: 'Address',
-      width: 150,
-      editable: true,
+    createdAt: 1555016400000,
+    status: 'pending'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1048',
+    amount: 25.1,
+    customer: {
+      name: 'Cao Yu'
     },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 110,
-      editable: true,
+    createdAt: 1555016400000,
+    status: 'delivered'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1047',
+    amount: 10.99,
+    customer: {
+      name: 'Alexa Richardson'
     },
-    {
-      field: 'Issue',
-      headerName: 'Issue',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.getValue(params.id, 'firstName') || ''} ${
-          params.getValue(params.id, 'lastName') || ''
-        }`,
+    createdAt: 1554930000000,
+    status: 'refunded'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1046',
+    amount: 96.43,
+    customer: {
+      name: 'Anje Keizer'
     },
-  ];
-  const rows = [
-    { id: 1, Name: 'Snow', Address: 'Jon', age: 35 },
-    { id: 2, Name: 'Lannister', Address: 'Cersei', age: 42 },
-    { id: 3, Name: 'Lannister', Address: 'Jaime', age: 45 },
-    { id: 4, Name: 'Stark', Addresse: 'Arya', age: 16 },
-    { id: 5, Name: 'Targaryen', Address: 'Daenerys', age: null },
-    { id: 6, Name: 'Melisandre', Address: null, age: 150 },
-    { id: 7, Name: 'Clifford', Address: 'Ferrara', age: 44 },
-    { id: 8, Name: 'Frances', Address: 'Rossini', age: 36 },
-    { id: 9, Name: 'Roxie', Address: 'Harvey', age: 65 },
-  ];
-  
-  export default function DataGridDemo() {
-    return (
-      <React.Fragment>
-              {/* <div style={{ height: 400, width: '100%' }}>
-                <DataGrid
-                  rows={rows}
-                  columns={columns}
-                  pageSize={5}
-                  rowsPerPageOptions={[5]}
-                  checkboxSelection
-                  disableSelectionOnClick
-                />
-              </div> */}
-              Help Requests
-      </React.Fragment>
-    );
+    createdAt: 1554757200000,
+    status: 'pending'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1045',
+    amount: 32.54,
+    customer: {
+      name: 'Clarke Gillebert'
+    },
+    createdAt: 1554670800000,
+    status: 'delivered'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1044',
+    amount: 16.76,
+    customer: {
+      name: 'Adam Denisov'
+    },
+    createdAt: 1554670800000,
+    status: 'delivered'
   }
+];
 
 
+
+
+
+const HelpRequests = (props) => (
+  <Card {...props}>
+    <CardHeader title="Latest Orders" />
+    <PerfectScrollbar>
+      <Box sx={{ minWidth: 800 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Order Ref
+              </TableCell>
+              <TableCell>
+                Customer
+              </TableCell>
+              <TableCell sortDirection="desc">
+                <Tooltip
+                  enterDelay={300}
+                  title="Sort"
+                >
+                  <TableSortLabel
+                    active
+                    direction="desc"
+                  >
+                    Date
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+              <TableCell>
+                Status
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((order) => (
+              <TableRow
+                hover
+                key={order.id}
+              >
+                <TableCell>
+                  {order.ref}
+                </TableCell>
+                <TableCell>
+                  {order.customer.name}
+                </TableCell>
+                <TableCell>
+                  {format(order.createdAt, 'dd/MM/yyyy')}
+                </TableCell>
+                <TableCell>
+                  <SeverityPill
+                    color={(order.status === 'delivered' && 'success')
+                    || (order.status === 'refunded' && 'error')
+                    || 'warning'}
+                  >
+                    {order.status}
+                  </SeverityPill>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </PerfectScrollbar>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        p: 2
+      }}
+    >
+      <Button
+        color="primary"
+        endIcon={<ArrowRightIcon fontSize="small" />}
+        size="small"
+        variant="text"
+      >
+        View all
+      </Button>
+    </Box>
+  </Card>
+);
+export default HelpRequests
