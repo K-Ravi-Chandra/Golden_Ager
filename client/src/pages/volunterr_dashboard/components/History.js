9d779b92ce6e6a2b89e69171561f87edbb6eaa48
AdityaCS19B019 <cs19b019@iittp.ac.in>
2021-11-12 21:30:41 +0530

    Dashboard updated completely

--- client/src/pages/volunterr_dashboard/components/History.js
+++ client/src/pages/volunterr_dashboard/components/History.js
@@ -1,78 +1,225 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React from 'react'
+import {format} from 'date-fns'
+import { v4 as uuid } from 'uuid';
+import PerfectScrollbar from 'react-perfect-scrollbar';
 
-export default function SimpleAccordion() {
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Tooltip
+} from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+
+const SeverityPillRoot = styled('span')(({ theme, ownerState }) => {
+  const backgroundColor = theme.palette[ownerState.color].main;
+  const color = theme.palette[ownerState.color].contrastText;
+
+  return {
+    alignItems: 'center',
+    backgroundColor,
+    borderRadius: 12,
+    color,
+    cursor: 'default',
+    display: 'inline-flex',
+    flexGrow: 0,
+    flexShrink: 0,
+    fontFamily: theme.typography.fontFamily,
+    fontSize: theme.typography.pxToRem(12),
+    lineHeight: 2,
+    fontWeight: 600,
+    justifyContent: 'center',
+    letterSpacing: 0.5,
+    minWidth: 20,
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    textTransform: 'uppercase',
+    whiteSpace: 'nowrap'
+  };
+});
+
+const SeverityPill = (props) => {
+  const { color = 'primary', children, ...other } = props;
+
+  const ownerState = { color };
+
   return (
-    <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Senior Citizen 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Issue of the Senior Citizen 1 accepted and solved
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Senior Citizen 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          Issue of the Senior Citizen 2 accepted and solved
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Senior Citizen 3</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          Issue of the Senior Citizen 3 accepted and solved
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Senior Citizen 4</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          Issue of the Senior Citizen 4 accepted and solved
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Issue of Senior Citizen 5 was not accepted</Typography>
-        </AccordionSummary>
-      </Accordion>
-    </div>
+    <SeverityPillRoot
+      ownerState={ownerState}
+      {...other}
+    >
+      {children}
+    </SeverityPillRoot>
   );
-}
+};
+
+SeverityPill.propTypes = {
+  children: PropTypes.node,
+  color: PropTypes.oneOf([
+    'primary',
+    'secondary',
+    'error',
+    'info',
+    'warning',
+    'success'
+  ])
+};
+
+
+const orders = [
+  {
+    id: uuid(),
+    ref: 'CDD1049',
+    amount: 30.5,
+    customer: {
+      name: 'Ekaterina Tankova'
+    },
+    createdAt: 1555016400000,
+    status: 'pending'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1048',
+    amount: 25.1,
+    customer: {
+      name: 'Cao Yu'
+    },
+    createdAt: 1555016400000,
+    status: 'accepted'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1047',
+    amount: 10.99,
+    customer: {
+      name: 'Alexa Richardson'
+    },
+    createdAt: 1554930000000,
+    status: 'rejected'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1046',
+    amount: 96.43,
+    customer: {
+      name: 'Anje Keizer'
+    },
+    createdAt: 1554757200000,
+    status: 'pending'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1045',
+    amount: 32.54,
+    customer: {
+      name: 'Clarke Gillebert'
+    },
+    createdAt: 1554670800000,
+    status: 'accepted'
+  },
+  {
+    id: uuid(),
+    ref: 'CDD1044',
+    amount: 16.76,
+    customer: {
+      name: 'Adam Denisov'
+    },
+    createdAt: 1554670800000,
+    status: 'rejected'
+  }
+];
+
+
+
+
+
+const History = (props) => (
+  <Card {...props}>
+    <CardHeader title="Latest Requests" />
+    <PerfectScrollbar>
+      <Box sx={{ minWidth: 800 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Name
+              </TableCell>
+              <TableCell>
+                Issue
+              </TableCell>
+              <TableCell sortDirection="desc">
+                <Tooltip
+                  enterDelay={300}
+                  title="Sort"
+                >
+                  <TableSortLabel
+                    active
+                    direction="desc"
+                  >
+                    Date
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+              <TableCell>
+                Status
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((order) => (
+              <TableRow
+                hover
+                key={order.id}
+              >
+                <TableCell>
+                  {order.ref}
+                </TableCell>
+                <TableCell>
+                  {order.customer.name}
+                </TableCell>
+                <TableCell>
+                  {format(order.createdAt, 'dd/MM/yyyy')}
+                </TableCell>
+                <TableCell>
+                  <SeverityPill
+                    color={(order.status === 'accepted' && 'success')
+                    || (order.status === 'rejected' && 'error')
+                    || 'warning'}
+                  >
+                    {order.status}
+                  </SeverityPill>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </PerfectScrollbar>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        p: 2
+      }}
+    >
+      <Button
+        color="primary"
+        endIcon={<ArrowRightIcon fontSize="small" />}
+        size="small"
+        variant="text"
+      >
+        View all
+      </Button>
+    </Box>
+  </Card>
+);
+export default History
