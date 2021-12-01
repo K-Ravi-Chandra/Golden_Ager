ada9fc9808623025463524e4902d94d9650983ba
K-Ravi-Chandra <cs19b020@iittp.ac.in>
2021-12-01 14:09:58 +0530

    Updated readability of - pages\dashboard\userdashboard

--- client/src/pages/dashboard/userdashboard.js
+++ client/src/pages/dashboard/userdashboard.js
@@ -1,3 +1,4 @@
+// All Imports ...
 import React from 'react'
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -6,69 +7,90 @@ import FamilyDashboard from '../family/FamilyDashboard';
 import DoctorDashboard from '../doctor_dashboard/DoctorDashboard'
 import VolunteerDashboard from '../volunterr_dashboard/VolunteerDashboard'
 
-const UserDashBoard = ({history}) => {
-    const [error, setError] = useState("");
-    const [privateData, setPrivateData] = useState("");
-    const [role, setRole] = useState('4');
+// UserDashboard
+const UserDashBoard = ({history}) => 
+{
+  const [error, setError] = useState("");
+  const [privateData, setPrivateData] = useState("");
+  const [role, setRole] = useState('4');
   
-    useEffect(() => {
+  useEffect(() =>
+  {
 
-    if(!localStorage.getItem("authToken")){
-            history.push("/login");
+    if(!localStorage.getItem("authToken"))
+    {
+      history.push("/login");
     }
-      const fetchPrivateDate = async () => {
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization" : `Bearer ${localStorage.getItem("authToken")}`,
-          },
-        };
-  
-        try {
-          const { data } = await axios.get("/api/private", config);
-          setPrivateData(data); 
-          setRole(data.role);
-          
-        } catch (error) {
-          localStorage.removeItem("authToken");
-          setError("You are not authorized please login");
-        }
+
+    const fetchPrivateDate = async () => 
+    {
+      const config = 
+      {
+        headers: 
+        {
+          "Content-Type": "application/json",
+          "Authorization" : `Bearer ${localStorage.getItem("authToken")}`,
+        },
       };
-  
-      fetchPrivateDate();
-    }, [history]);
-
-    const logout = () => {
+      
+      // In case of unauthorized login , it tells to login to the page 
+      try 
+      {
+        const { data } = await axios.get("/api/private", config);
+        setPrivateData(data); 
+        setRole(data.role);
+      } 
+      catch (error) 
+      {
         localStorage.removeItem("authToken");
-        history.push("/");
-    }
-    const renderSwitch = (role) => {
-      switch(role) {
-        
-        case '0':
-          return <VolunteerDashboard data={privateData}/>
-   
-        case '1':
-          return <SeniorCitizenDashboard data={privateData}/>   
-        
-        case '2':
-          return <DoctorDashboard data={privateData}/>
-        
-        case '3':
-          return <FamilyDashboard data={privateData}/> 
-
-        default:
-          return <div></div>;
+        setError("You are not authorized please login");
       }
-    }
-    
-    return error ? (
-      <span className="error-message">{error}</span>
-    ) : (
-        <>
-        {renderSwitch(role)}
-        </>
-    );
-  };
+
+    };
   
-  export default UserDashBoard;
+    fetchPrivateDate();
+
+  }, [history]);
+
+  const logout = () =>
+  {
+      localStorage.removeItem("authToken");
+      history.push("/");
+  }
+  
+  // Switch cases for entering all Dashboards
+  const renderSwitch = (role) => 
+  {
+    switch(role)
+    {
+      case '0':
+        return <VolunteerDashboard data={privateData}/>
+   
+      case '1':
+        return <SeniorCitizenDashboard data={privateData}/>   
+        
+      case '2':
+        return <DoctorDashboard data={privateData}/>
+       
+      case '3':
+        return <FamilyDashboard data={privateData}/> 
+
+      default:
+        return <div></div>;
+    }
+  }
+    
+  return error ? 
+  (
+    <span className="error-message"> {error} </span>
+  ) : 
+  
+  (
+    <>
+      {renderSwitch(role)}
+    </>
+  );
+
+};
+  
+export default UserDashBoard;
