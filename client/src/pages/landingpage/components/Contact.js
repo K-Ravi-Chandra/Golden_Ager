c8cda18bba28698af678ebe9dde969ec706ed173
K-Ravi-Chandra <cs19b020@iittp.ac.in>
2021-12-02 12:22:24 +0530

    Updated Readability of all pages in landing page of landingpage

--- client/src/pages/landingpage/components/Contact.js
+++ client/src/pages/landingpage/components/Contact.js
@@ -1,25 +1,30 @@
+// All Imports..-------------------------------------------------------
 import React, { useState } from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import Logo from '../../../components/logo.png'
 import { styled} from '@mui/styles';
+
 import { AppBar, Box ,Toolbar,Typography, Stack , Button, Paper, Container, Grid} from '@mui/material'
 import Textfield from '../../../components/formUI/Textfield';
 import SubmitButton from '../../../components/formUI/Button';
 import Alert from '@mui/material/Alert';
 import IconButton from '@mui/material/IconButton';
+
 import Collapse from '@mui/material/Collapse';
 import CloseIcon from '@mui/icons-material/Close';
 import AlertTitle from '@mui/material/AlertTitle';
 import axios from "axios";
 import Dialog from '@mui/material/Dialog';
+
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { blue, grey } from '@mui/material/colors';
 
-
+// ----------------------------------------------------------------------------
+// Contact page
 
 const Contact = () => {
 
@@ -29,13 +34,14 @@ const Contact = () => {
     setOpen(false);
   };
 
-
+  // Null by default
   const INITIAL_FORM_STATE = {
     name: '',
     email: '',
     message: '',
   };
   
+  // Accepting inputs and restricting in case of invalid inputs
   const FORM_VALIDATION = Yup.object().shape({
     name: Yup.string()
       .required('Required'),
@@ -54,7 +60,8 @@ const Contact = () => {
       },
     };
 
-    try {
+    try 
+    {
       const { data } = await axios.post(
         "/api/contact",
         {values},
@@ -66,108 +73,114 @@ const Contact = () => {
 
       props.resetForm()
 
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       console.log(error);
     }
   }
   
+  // Final return 
+  // Adding all style components
+  return (
+    <Paper sx={{position: 'relative',backgroundColor: grey[200],color: 'black',}}>
+      
+      <Box sx={{position: 'relative',p: 5,}}>
 
-  
-  
+          <Typography
+            component="h1"
+            variant="h4"
+            align="center"
+            color="inherit"
+            gutterBottom
+            paragraph
+          >
+            Contact us
 
-    return (
-        <Paper sx={{position: 'relative',backgroundColor: grey[200],color: 'black',}}>
-                <Box sx={{position: 'relative',p: 5,}}>
-                <Typography
-                  component="h1"
-                  variant="h4"
-                  align="center"
-                  color="inherit"
-                  gutterBottom
-                  paragraph
+          </Typography>
+
+          <Formik
+            initialValues={{
+              ...INITIAL_FORM_STATE
+            }}
+
+            validationSchema={FORM_VALIDATION}
+            onSubmit={onSubmit}
+          >
+              <Form>
+
+                <Grid container spacing={2} rowSpacing = {2}>
+
+                  <Grid item xs={12}>
+                    <Textfield
+                      required
+                      name="name"
+                      label="Name"
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield
+                      required
+                      name="email"
+                      label="Email"
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield
+                      required
+                      name="message"
+                      label="Message"
+                      multiline={true}
+                      rows={4}
+                    />
+                  </Grid>
+
+                </Grid>
+
+                <Stack
+                  sx={{ p: 2}}
+                  direction="row"
+                  spacing={2}
+                  justifyContent="center"
                 >
-                    Contact us
-                </Typography>
+                  <SubmitButton>Submit</SubmitButton>
+                
+                </Stack>
 
-
-                <Formik
-                      initialValues={{
-                        ...INITIAL_FORM_STATE
-                      }}
-                      validationSchema={FORM_VALIDATION}
-                      onSubmit={onSubmit}
-                    >
-                      <Form>
-
-                        <Grid container spacing={2} rowSpacing = {2}>
-
-
-                          <Grid item xs={12}>
-                            <Textfield
-                              required
-                              name="name"
-                              label="Name"
-                            />
-                          </Grid>
-
-
-
-                          <Grid item xs={12}>
-                            <Textfield
-                            required
-                              name="email"
-                              label="Email"
-                            />
-                          </Grid>
-
-
-                          <Grid item xs={12}>
-                            <Textfield
-                            required
-                              name="message"
-                              label="Message"
-                              multiline={true}
-                              rows={4}
-                            />
-                          </Grid>
-
-                        </Grid>
-
-                        <Stack
-                            sx={{ p: 2}}
-                            direction="row"
-                            spacing={2}
-                            justifyContent="center"
-                          >
-                            <SubmitButton>Submit</SubmitButton>
-                          </Stack>
-
-                      </Form>
-                    </Formik>
+              </Form>
+          </Formik>
             
-          </Box>
-          <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-            <DialogTitle id="alert-dialog-title">
+      </Box>
+      
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+          <DialogTitle id="alert-dialog-title">
               {"Submitted Successfully"}
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-              your form has been received. We will get back to you quickly.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose} autoFocus>
-                Ok
-              </Button>
-            </DialogActions>
+          </DialogTitle>
+      
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Your form has been received. We will get back to you quickly.
+            </DialogContentText>
+          </DialogContent>
+          
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              Ok
+            </Button> 
+          </DialogActions>
+      
       </Dialog>
+
     </Paper>
-    )
+  )
+
 }
 
 export default  Contact
--- client/src/pages/landingpage/components/Navigation.js
+++ client/src/pages/landingpage/components/Navigation.js
@@ -1,24 +1,31 @@
+// All Imports ----------------------------------------------------------
 import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import { useTheme, useMediaQuery, Menu, MenuItem , Link} from '@mui/material';
+
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+
 import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Logo from './logo.png';
 import { styled} from '@mui/styles';
 
+//-----------------------------------------------------------------------------
+
+// Adding style components - buttons
 
 const Title = styled(Typography)({
   fontSize : 30,
@@ -28,8 +35,8 @@ const Title = styled(Typography)({
   WebkitTextFillColor: "transparent"
 });
 
-export default function NavBar() {
-
+export default function NavBar() 
+{
   // Drawer
   const isMobile = useMediaQuery(useTheme().breakpoints.down("sm"))
   const [state, setState] = useState(false); // state of drawer
@@ -41,47 +48,52 @@ export default function NavBar() {
     setState(open);
   };
 
-
-
   const drawer = (
-    <div>
-        <Toolbar sx={{ toolbar: (theme) => theme.mixins.toolbar }}/>
-        <List>
-                <ListItemButton component={Link} href="https://rzp.io/l/yt2s6Yf">
-                  <ListItemText  primary="Donate Money" />
-                </ListItemButton> 
+      <div>
+
+          <Toolbar sx={{ toolbar: (theme) => theme.mixins.toolbar }}/>
+
+          <List>
+              <ListItemButton component={Link} href="https://rzp.io/l/yt2s6Yf">
+                <ListItemText  primary="Donate Money" />
+              </ListItemButton> 
                 
-                <ListItemButton component={Link} href="/donaterequirements" >
-                  <ListItemText primary="Donate Requirements" />
-                </ListItemButton>
+              <ListItemButton component={Link} href="/donaterequirements" >
+                <ListItemText primary="Donate Requirements" />
+              </ListItemButton>
 
-                <Divider/>
+              <Divider/>
 
-                <ListItemButton  component={Link} href="/login" >
-                          <ListItemText  primary={
-                              <Typography color="primary">
-                                LOGIN
-                              </Typography>
-                            } 
-                          />
-                </ListItemButton>
+              <ListItemButton  component={Link} href="/login" >
+                <ListItemText  primary={
+                  <Typography color="primary">
+                    LOGIN
+                  </Typography>
+                  } 
+                />
+              
+              </ListItemButton>
 
-                <ListItemButton  component={Link} href ="/register" >
-                          <ListItemText  primary={
-                              <Typography color="primary" >
-                                REGISTER
-                              </Typography>
-                            } 
-                          />
-                  </ListItemButton>
-        </List>
-    </div>
+              <ListItemButton  component={Link} href ="/register" >
+                <ListItemText  primary={
+                    <Typography color="primary" >
+                      REGISTER
+                    </Typography>
+                    } 
+                />
+              </ListItemButton>
+
+          </List>
+
+      </div>
     
   )
 
-  // Appbar Menu
+  // App-bar Menu
+
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -89,81 +101,86 @@ export default function NavBar() {
     setAnchorEl(null);
   };
 
-
+  // Final output return
   return (
-
     <React.Fragment>
+      
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar elevation ={0} color= "inherit"  sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar >
+
+        <AppBar elevation ={0} color= "inherit"  sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar >
           
             <Link underline="none" href="/">
               <img style = {{ width :32, height :32, margin : 4}} alt = "logo" src = {Logo} />
             </Link>
           
             <Box sx={{ flexGrow: 1 }}>
-            <Title as={Link} underline="none" href="/" variant="h5" >
+              <Title as={Link} underline="none" href="/" variant="h5" >
            
                   Golden Ager
             
-          </Title>
+              </Title>
             </Box>
 
           
-          {isMobile ? (<></>
-            
+            {isMobile ? (<></>
             ) : (
+
               <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'right',
-          width: 'fit-content',
-          edge: 'right'
-        }}
-      >
+                sx={{
+                  display: 'flex',
+                  alignItems: 'right',
+                  width: 'fit-content',
+                  edge: 'right'
+                }}
+              >
 
-          <Button onClick={handleClick} sx={{mx:1}} color="inherit"> Donate </Button>
-          <Divider orientation="vertical" flexItem />
-          <Button color="primary" component={Link} href="/login">
-                Login
-          </Button>
-          <Button color="primary" variant = "outlined" component={Link} href="/register" >Signup</Button>
+                  <Button onClick={handleClick} sx={{mx:1}} color="inherit"> Donate </Button>
+                  <Divider orientation="vertical" flexItem />
+                
+                  <Button color="primary" component={Link} href="/login">
+                    Login
+                  </Button>
+          
+                  <Button color="primary" variant = "outlined" component={Link} href="/register" >Signup</Button>
 
+              </Box>
+
+            )}
+          
+            <IconButton
+              color="inherit"
+              edge = "end"
+              aria-label="menu"
+              onClick={toggleDrawer(!state)}
+              sx={{  display: { sm: 'none' } }}
+            >
+              {state ? <CloseIcon/> : <MenuIcon /> }
+            
+            </IconButton>
+         
+            <Drawer
+              anchor='top'
+              open={state && isMobile}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              onClose={toggleDrawer(!state)}
+            >
+              {drawer}
+            </Drawer>
+        
+          </Toolbar>
+        </AppBar>
+      
       </Box>
 
-
-
-          )}
-          
-          <IconButton
-            color="inherit"
-            edge = "end"
-            aria-label="menu"
-            onClick={toggleDrawer(!state)}
-            sx={{  display: { sm: 'none' } }}
-          >
-            {state ? <CloseIcon/> : <MenuIcon /> }
-            
-          </IconButton>
-          <Drawer
-            anchor='top'
-            open={state && isMobile}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            onClose={toggleDrawer(!state)}
-          >
-            {drawer}
-          </Drawer>
-        </Toolbar>
-      </AppBar>
-      
-    </Box>
-    <Menu
+      <Menu
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
         onClick={handleClose}
+
         PaperProps={{
           elevation: 0,
           sx: {
@@ -171,29 +188,32 @@ export default function NavBar() {
             filter: 'drop-shadow(0px 2px 8px rgba(50,50,110,0.32))',
             mt: 1.5,
             '& .MuiAvatar-root': {
-              width: 32000,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
+                width: 32000,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
             },
             '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
             },
+
           },
+
         }}
+
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-         <Link as={MenuItem } underline="none" target="_blank"  href = {'https://rzp.io/l/yt2s6Yf'} >
+        <Link as={MenuItem } underline="none" target="_blank"  href = {'https://rzp.io/l/yt2s6Yf'} >
           Donate Money
         </Link> 
 
@@ -201,8 +221,10 @@ export default function NavBar() {
           Donate Requirements
         </MenuItem>
         
-    </Menu>
+      </Menu>
+
     </React.Fragment>
   );
+
 }
 
